78072f8eb6a8511d39597a4210ff94cf407b5df4
palak-rane <107403370+palak-rane@users.noreply.github.com>
2025-04-30 22:27:20 +0100

    post api request

---
+++ tests/post_api_request_02.spec.js
@@ -0,0 +1,24 @@
+const{test,expect }= require('@playwright/test');
+const bookingAPIRequestBody=require('../test-Data/post_request_body.json')
+test("Post api request using static json file", async({request})=>{
+   
+    const postAPIResponse = await request.post('/booking',{
+        data: bookingAPIRequestBody
+           
+    })
+
+    //Validate status code
+    expect(postAPIResponse.ok()).toBeTruthy();
+    expect(postAPIResponse.status()).toBe(200);
+   const postAPIResponseBody = await postAPIResponse.json();
+   console.log(postAPIResponseBody);
+
+   //validate json repsonse
+   expect(postAPIResponseBody.booking).toHaveProperty("firstname","Playwright");
+   expect(postAPIResponseBody.booking).toHaveProperty("lastname", "API Testing")
+
+   //validate nested json response
+   expect(postAPIResponseBody.booking.bookingdates).toHaveProperty("checkin", "2018-01-01");
+   expect(postAPIResponseBody.booking.bookingdates).toHaveProperty("checkout", "2019-01-01")
+})
+
